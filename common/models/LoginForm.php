b33360556c2e66ccb69243f089f784297868eece
Damien Cranon <dame.cranon@gmail.com>
2015-08-22 16:36:11 -0400

    Changed a line

--- common/models/LoginForm.php
+++ common/models/LoginForm.php
@@ -66,12 +66,16 @@ class LoginForm extends Model
 
     public function loginAdmin()
     {
-           if (($this->validate()) && PermissionHelpers::requireMinimumRole('Admin', $this->getUser()->id)) {
-                 return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 * 30 : 0);
+        if (($this->validate()) && PermissionHelpers::requireMinimumRole('Admin', $this->getUser()->id)) {
 
-           } else {
-               throw new NotFoundHttpException('You Shall Not Pass');
-           }
+            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 * 30 : 0);
+
+        } else {
+
+            throw new NotFoundHttpException('You Shall Not Pass.');
+
+        }
+
     }
 
     /**
